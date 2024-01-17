91f683c27fd834adf7461dd6bcb3ccef0c1891c8
naviluis <129198266+naviluis@users.noreply.github.com>
2024-01-16 21:25:33 -0600

    Update backend.tf

--- environments/us-east-1/backend.tf
+++ environments/us-east-1/backend.tf
@@ -1,12 +1,12 @@
 terraform {
   
   backend "s3" {
-    bucket         = ${{secrets.BUCKET_NAME}}
-    dynamodb_table = ${{secrets.DYNAMO_TABLE}}
-    key            = ${{secrets.FILE_KEY}}
+    bucket         = "${{secrets.BUCKET_NAME}}"
+    dynamodb_table = "${{secrets.DYNAMO_TABLE}}"
+    key            = "${{secrets.FILE_KEY}}"
     encrypt        = true
-    kms_key_id     = ${{secrets.KMS_KEY_ID}}
+    kms_key_id     = "${{secrets.KMS_KEY_ID}}"
     region         = "us-east-1"
-    profile        = ${{secrets.PROFILE}}
+    profile        = "${{secrets.PROFILE}}"
   }
 }

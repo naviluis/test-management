6708068e6ebc99b0b0abc25d90d43304f26947fb
naviluis <lcerdas@navisite.com>
2024-01-18 12:43:40 -0600

    Testing new way of doing tf init 2

---
+++ environments/us-east-1/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+ backend "s3" {  
+    bucket         = "[[BUCKET_NAME]]"
+    dynamodb_table = "[[DYNAMO_TABLE]]"
+    key            = "[[FILE_KEY]]"
+    encrypt        = true
+    kms_key_id     = "[[KMS_KEY_ID]]"
+    region         = "us-east-1"
+    profile        = "[[PROFILE]]"
+ } 
+}
